c13adab68ac36de715e37ba4012b0865626c394c
athena <biplab.mal@keysight.com>
2021-09-16 10:53:02 +0530

    operator tests submodule added

--- do.sh
+++ do.sh
@@ -24,6 +24,19 @@ ARISTA_CEOS_VERSION=4.26.1F
 
 GCP_DOCKER_REPO=us-central1-docker.pkg.dev/kt-nts-athena-dev/keysight
 
+LOCK_STATUS_FILE=sanity_lock_status.txt
+LOCK_FILE=sanity_lock
+
+SANITY_REPORTS=./reports
+SANITY_LOGS=./logs
+SANITY_STATUS=FAIL
+
+START_TIME="$(date -u +%s)"
+TOTAL_TIME=7200
+APPROX_SANITY_TIME=900
+
+TESTBED_CICD_DIR=operator_cicd
+
 # get installers based on host architecture
 if [ "$(arch)" = "aarch64" ] || [ "$(arch)" = "arm64" ]
 then
@@ -223,10 +236,155 @@ cicd_gen_local_ixia_c_artifacts() {
     echo "Files in ./ixia_c_art: $(ls -lht ${ixia_c_art})"
 }
 
+cicd_exec_on_testbed() {
+    cmd=${1}
+    sshpass -p ${TESTBED_PASSWORD} ssh -o StrictHostKeyChecking=no  ${TESTBED_USERNAME}@${TESTBED} "${cmd}"
+}
+
+cicd_create_lock_status_file() {
+    echo "creating lock status file: ${LOCK_STATUS_FILE}"
+    touch ${LOCK_STATUS_FILE}
+    echo "lock status file: ${LOCK_STATUS_FILE} created"
+}
+
+cicd_create_folder_in_testbed() {
+    echo "creating cicd folder in testbed: ${TESTBED_CICD_DIR}"
+    cicd_exec_on_testbed "mkdir ${TESTBED_CICD_DIR}"
+    echo "creating cicd folder: ${TESTBED_CICD_DIR}"
+}
+
+cicd_lock_testbed() {
+    echo "Locking testbed for sanity"
+    cicd_create_folder_in_testbed
+    cicd_create_lock_status_file
+    echo "Locked testbed for sanity"
+}
+
+cicd_check_for_timeout() {
+    echo "Checking for timeout in CICD Pipeline"
+    END_TIME="$(date -u +%s)"
+    ELAPSED="$(($END_TIME-$START_TIME))"
+    echo "Time Elapsed: ${ELAPSED} seconds"
+    echo "Timeout Given For Pipeline: ${TOTAL_TIME} seconds"
+    REM_TIME="$(($TOTAL_TIME-$ELAPSED))"
+    echo "Remaining time for pipeline : ${REM_TIME} seconds"
+    echo "Estimated Time required to run sanity ${APPROX_SANITY_TIME} seconds"
+    if [[ ${REM_TIME} -lt ${APPROX_SANITY_TIME} ]]
+    then
+        echo "Pipeline may expire during running tests in testbed, please retry the pipeline sometimes later"
+        exit 1
+    fi
+    echo "Pipeline won't expire during running tests in testbed"
+
+}
+
+cicd_wait_for_testbed_to_unlock() {
+    local_lock_status=$(ls ${LOCK_STATUS_FILE} 2> /dev/null)
+    if [[ ${local_lock_status} ]]
+    then
+        echo "Local lock status file forund, so testbed is already locked by this process"
+    else
+        while :
+        do
+            status=$(cicd_exec_on_testbed "ls -d ${TESTBED_CICD_DIR} 2> /dev/null")
+            if  [[ ${status} ]]
+            then
+                echo "Testbed is locked due to another sanity is still running....."
+                echo "Retry after 1 minute"
+                sleep 1m
+            else
+                echo "Testbed is already unlocked"
+                cicd_check_for_timeout
+                cicd_lock_testbed
+                break
+            fi
+        done 
+    fi  
+}
+
+cicd_copy_file_to_testbed() {
+    for var in "$@"
+    do
+        echo "pushing ${var} to testbed"
+        sshpass -p ${TESTBED_PASSWORD} scp -o StrictHostKeyChecking=no ${var} ${TESTBED_USERNAME}@${TESTBED}:./${TESTBED_CICD_DIR}/
+        echo "${var} pushed to testbed"
+    done
+}
+
+cicd_push_artifacts_to_testbed() {
+    art=${1}
+    cicd_copy_file_to_testbed ./art/*
+    cicd_copy_file_to_testbed ./ixia_c_art/*
+    cicd_copy_file_to_testbed ./tests_art/*
+}
+
+cicd_run_sanity_in_testbed() {
+    version=${1}
+    echo "sanity run in testbed: starting"
+    sanity_run_cmd="python3 operator_cicd.py --test -build ${version} -mark sanity"
+    cicd_exec_on_testbed "cd ./${TESTBED_CICD_DIR} && sudo -S <<< ${TESTBED_PASSWORD} ${sanity_run_cmd}"
+    echo "sanity run in testbed: done"
+}
+
+cicd_copy_results_from_testbed() {
+    for var in "$@"
+    do
+        echo "pulling ${var} from testbed"
+        sshpass -p ${TESTBED_PASSWORD} scp -o StrictHostKeyChecking=no -r ${TESTBED_USERNAME}@${TESTBED}:/home/${TESTBED_USERNAME}/${TESTBED_CICD_DIR}/${var} ${SANITY_REPORTS}/
+        echo "${var} pulled from testbed"
+    done
+}
+
+cicd_pull_results_from_testbed() {
+    version=${1}
+    mkdir -p ${SANITY_REPORTS}
+    cicd_copy_results_from_testbed reports-${version}-* sanity-summary-${version}.csv
+}
+
+cicd_cleanup_in_testbed() {
+    echo "clean up in testbed: starting"
+    cicd_exec_on_testbed "cd ${TESTBED_CICD_DIR} && sudo -S <<< ${TESTBED_PASSWORD} python3 operator_cicd.py --clean"
+    cicd_exec_on_testbed "cd ${TESTBED_CICD_DIR} && sudo -S <<< ${TESTBED_PASSWORD} rm operator_cicd.py"
+    echo "clean up in testbed: done"
+}
+
+cicd_check_sanity_status() {
+    version=${1}
+    echo "checking santy status"
+    cat ${SANITY_REPORTS}/sanity-summary-${version}.csv
+    sanity_pass=$(grep All ${SANITY_REPORTS}/sanity-summary-${version}.csv | grep -Eo '[0-9]{1,4}')
+    echo "Sanity pass rate : ${sanity_pass}"
+    sanity_pass_rate=$(echo "${sanity_pass}" | tr -d $'\r' | bc -l)
+    if [[ ${sanity_pass_rate} -ge ${EXPECTED_SANITY_PASS_RATE} ]]
+    then
+        SANITY_STATUS=PASS
+    fi
+    echo "Sanity expected pass rate : ${EXPECTED_SANITY_PASS_RATE}"
+    if [[ ${SANITY_STATUS} == PASS ]]
+    then
+        echo "sanity : pass"
+    else 
+        echo "sanity: fail"
+        exit 1
+    fi
+}
+
+cicd_run_sanity() {
+    art=${1}
+    version=${2}
+    cicd_push_artifacts_to_testbed ${art}
+    cicd_run_sanity_in_testbed ${version}
+    cicd_pull_results_from_testbed ${version}
+    cicd_cleanup_in_testbed
+
+    echo "Reports in ${SANITY_REPORTS}: $(ls -lht ${SANITY_REPORTS})"
+    cicd_check_sanity_status ${version}
+}
+
 cicd_install_deps() {
     echo "Installing CICD deps"
     apk update \
-    && apk add curl git openssh vim unzip tar make bash wget \
+    && apk add curl git openssh vim unzip tar make bash wget sshpass \
     && apk add --no-cache libc6-compat \
     && apk add build-base
 
@@ -254,12 +412,49 @@ cicd () {
 
     cicd_gen_local_ixia_c_artifacts
 
+    # pipeline wait for testbed to be unlocked for sanity
+    cicd_wait_for_testbed_to_unlock \
+    && cicd_run_sanity ${art} ${version}
+
     if [ ${CI_COMMIT_REF_NAME} = "main" ]
     then 
         cicd_publish_to_docker_repo ${version}
         cicd_publish_to_generic_repo ${art} ${version}
     fi
     docker rmi -f ${IXIA_C_OPERATOR_IMAGE}:${version} 2> /dev/null || true
+}
+
+
+remove_cicd_folder_from_testbed(){
+    lock_status=$(ls ${LOCK_STATUS_FILE} 2> /dev/null)
+    if [[ ${lock_status} ]]
+    then
+        status=$(cicd_exec_on_testbed "ls -d ${TESTBED_CICD_DIR} 2> /dev/null")
+        if  [[ ${status} ]]
+        then
+            cicd_exec_on_testbed "sudo -S <<< ${TESTBED_PASSWORD} rm -rf ${TESTBED_CICD_DIR}" 
+            echo "${TESTBED_CICD_DIR}: deletd from testbed"
+        else
+            echo "${TESTBED_CICD_DIR}: not found in testbed"
+        fi
+    fi
+}
+
+remove_testbed_lock_status() {
+    echo "removing testbed lock status file"
+    lock_status=$(ls ${LOCK_STATUS_FILE} 2> /dev/null)
+    if [[ ${lock_status} ]]
+    then 
+        rm ${LOCK_STATUS_FILE}
+        echo "testbed lock status file removed"
+    fi
+}
+
+unlock_testbed(){
+    echo "unlocking testbed"
+    remove_cicd_folder_from_testbed
+    remove_testbed_lock_status
+    echo "testbed unlocked"
 }
 
 case $1 in
