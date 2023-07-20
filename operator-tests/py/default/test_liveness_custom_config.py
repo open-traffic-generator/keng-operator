6a1b94359c374f10537f63b274ad5d5550f103d1
anjan-keysight <anjan.roy@keysight.com>
2023-07-20 11:04:08 +0000

    Updated default parameters for liveness probe

--- operator-tests/py/default/test_liveness_custom_config.py
+++ operator-tests/py/default/test_liveness_custom_config.py
@@ -23,7 +23,7 @@ def test_liveness_custom_config():
         '-protocol-engine',
         '-traffic-engine'
     ]
-    probe_params = {'protocol-engine':{'liveness-initial-delay': 12}, 'traffic-engine':{'liveness-period': 10}, 'controller':{'liveness-failure': 3}, 'gnmi-server':{'liveness-enable': False}}
+    probe_params = {'protocol-engine':{'liveness-initial-delay': 12}, 'traffic-engine':{'liveness-period': 5}, 'controller':{'liveness-failure': 3}, 'gnmi-server':{'liveness-enable': False}}
     try:
         op_rscount = utils.get_operator_restart_count()
         print("[Namespace:{}]Deploying KNE topology".format(
@@ -32,12 +32,12 @@ def test_liveness_custom_config():
         utils.load_liveness_configmap(probe_params)
         utils.create_kne_config(namespace1_config, namespace1)
         utils.ixia_c_pods_ok(namespace1, expected_pods)
-        utils.check_liveness_data('ixia-c', expected_pods[0], namespace1, True, 10, 20, 3)
+        utils.check_liveness_data('ixia-c', expected_pods[0], namespace1, True, 1, 10, 3)
         utils.check_liveness_data('gnmi', expected_pods[0], namespace1, False)
-        utils.check_liveness_data(expected_pods[1]+container_extensions[0], expected_pods[1], namespace1, True, 12, 20, 10)
-        utils.check_liveness_data(expected_pods[1]+container_extensions[1], expected_pods[1], namespace1, True, 10, 10, 10)
-        utils.check_liveness_data(expected_pods[2]+container_extensions[0], expected_pods[2], namespace1, True, 12, 20, 10)
-        utils.check_liveness_data(expected_pods[2]+container_extensions[1], expected_pods[2], namespace1, True, 10, 10, 10)
+        utils.check_liveness_data(expected_pods[1]+container_extensions[0], expected_pods[1], namespace1, True, 12, 10, 6)
+        utils.check_liveness_data(expected_pods[1]+container_extensions[1], expected_pods[1], namespace1, True, 1, 5, 6)
+        utils.check_liveness_data(expected_pods[2]+container_extensions[0], expected_pods[2], namespace1, True, 12, 10, 6)
+        utils.check_liveness_data(expected_pods[2]+container_extensions[1], expected_pods[2], namespace1, True, 1, 5, 6)
         op_rscount = utils.ixia_c_operator_ok(op_rscount)
 
         print("[Namespace:{}]Deleting KNE topology".format(
