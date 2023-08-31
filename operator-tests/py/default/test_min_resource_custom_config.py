077700392e235f01a444f56317bbddded8476951
anjan-keysight <anjan.roy@keysight.com>
2023-08-31 23:09:47 +0000

    Unit tests added

---
+++ operator-tests/py/default/test_min_resource_custom_config.py
@@ -0,0 +1,59 @@
+import pytest
+import utils
+import time
+
+
+def test_min_resource_custom_config():
+    """
+    Deploy b2b kne topology with default version,
+    - namespace - 1: ixia-c
+    Delete b2b kne topology,
+    - namespace - 1: ixia-c
+    Validate,
+    - default minimum resource for all components
+    """
+    namespace1 = 'ixia-c'
+    namespace1_config = 'ixia_c_default_config.txt'
+    expected_pods = [
+        'otg-controller',
+        'otg-port-eth1',
+        'otg-port-eth2'
+    ]
+    container_extensions = [
+        '-protocol-engine',
+        '-traffic-engine'
+    ]
+    try:
+        custom_resource_params = {'protocol-engine':{'cpu': '300m','memory': '50Mi'}, 'traffic-engine':{'cpu': '50m', 'memory': '170Mi'}, 'controller':{'cpu': '50m', 'memory': '190Mi'}, 'gnmi-server':{'cpu': '70m', 'memory': '90Mi'}}
+        op_rscount = utils.get_operator_restart_count()
+        print("[Namespace:{}]Deploying KNE topology".format(
+            namespace1
+        ))
+        utils.load_min_resource_configmap(custom_resource_params)
+        utils.create_kne_config(namespace1_config, namespace1)
+        utils.ixia_c_pods_ok(namespace1, expected_pods)
+        utils.check_min_resource_data('ixia-c', expected_pods[0], namespace1, '190Mi', '50m')
+        utils.check_min_resource_data('gnmi', expected_pods[0], namespace1, '90Mi', '70m')
+        utils.check_min_resource_data(expected_pods[1]+container_extensions[0], expected_pods[1], namespace1, '50Mi', '300m')
+        utils.check_min_resource_data(expected_pods[1]+container_extensions[1], expected_pods[1], namespace1, '170Mi', '50m')
+        utils.check_min_resource_data(expected_pods[2]+container_extensions[0], expected_pods[2], namespace1, '50Mi', '300m')
+        utils.check_min_resource_data(expected_pods[2]+container_extensions[1], expected_pods[2], namespace1, '170Mi', '50m')
+        op_rscount = utils.ixia_c_operator_ok(op_rscount)
+
+        print("[Namespace:{}]Deleting KNE topology".format(
+            namespace1
+        ))
+        utils.delete_kne_config(namespace1_config, namespace1)
+        utils.ixia_c_pods_ok(namespace1, [])
+        op_rscount = utils.ixia_c_operator_ok(op_rscount)
+
+    finally:
+        utils.delete_kne_config(namespace1_config, namespace1)
+        utils.ixia_c_pods_ok(namespace1, [])
+
+        utils.wait_for(
+            lambda: utils.topology_deleted(namespace1),
+            'topology deleted',
+            timeout_seconds=30
+        )
+        time.sleep(5)
