89d1dd5a27205b297fc113419a65e8492eee5132
anjan-keysight <anjan.roy@keysight.com>
2024-01-11 21:20:05 +0000

    Fix liveness probe for license server

--- operator-tests/py/default/test_liveness_default_config.py
+++ operator-tests/py/default/test_liveness_default_config.py
@@ -23,15 +23,19 @@ def test_liveness_default_config():
         '-protocol-engine',
         '-traffic-engine'
     ]
+    lic_path = 'docker-local-athena.artifactory.it.keysight.com/keng-license-server'
+    lic_tag = '0.0.1-32'
     try:
         op_rscount = utils.get_operator_restart_count()
         print("[Namespace:{}]Deploying KNE topology".format(
             namespace1
         ))
+        utils.load_license_configmap("", lic_path, lic_tag)
         utils.create_kne_config(namespace1_config, namespace1)
         utils.ixia_c_pods_ok(namespace1, expected_pods)
         utils.check_liveness_data('ixia-c', expected_pods[0], namespace1, True, 1, 10, 6)
         utils.check_liveness_data('gnmi', expected_pods[0], namespace1, True, 1, 10, 6)
+        utils.check_liveness_data('license-server', expected_pods[0], namespace1, True, 1, 10, 6)
         utils.check_liveness_data(expected_pods[1]+container_extensions[0], expected_pods[1], namespace1, True, 1, 10, 6)
         utils.check_liveness_data(expected_pods[1]+container_extensions[1], expected_pods[1], namespace1, True, 1, 10, 6)
         utils.check_liveness_data(expected_pods[2]+container_extensions[0], expected_pods[2], namespace1, True, 1, 10, 6)
@@ -46,6 +50,7 @@ def test_liveness_default_config():
         op_rscount = utils.ixia_c_operator_ok(op_rscount)
 
     finally:
+        utils.reset_configmap()
         utils.delete_kne_config(namespace1_config, namespace1)
         utils.ixia_c_pods_ok(namespace1, [])
 
