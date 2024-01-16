4f1892f987fac4d748d94bf22428d3665488d8a3
anjan-keysight <anjan.roy@keysight.com>
2024-01-16 10:16:44 +0000

    Update default deployment test

--- operator-tests/py/default/test_liveness_default_config.py
+++ operator-tests/py/default/test_liveness_default_config.py
@@ -10,7 +10,8 @@ def test_liveness_default_config():
     Delete b2b kne topology,
     - namespace - 1: ixia-c
     Validate,
-    - default liveness parameters for protocol engines
+    - default liveness parameters for all ixia-c components
+    - no flakiness for default deployment of all components
     """
     namespace1 = 'ixia-c'
     namespace1_config = 'ixia_c_default_config.txt'
@@ -25,28 +26,36 @@ def test_liveness_default_config():
     ]
     lic_path = 'docker-local-athena.artifactory.it.keysight.com/keng-license-server'
     lic_tag = '0.0.1-32'
+    count = 30
     try:
         op_rscount = utils.get_operator_restart_count()
-        print("[Namespace:{}]Deploying KNE topology".format(
-            namespace1
-        ))
         utils.load_license_configmap("", lic_path, lic_tag)
-        utils.create_kne_config(namespace1_config, namespace1)
-        utils.ixia_c_pods_ok(namespace1, expected_pods)
-        utils.check_liveness_data('ixia-c', expected_pods[0], namespace1, True, 1, 10, 6)
-        utils.check_liveness_data('gnmi', expected_pods[0], namespace1, True, 1, 10, 6)
-        utils.check_liveness_data('license-server', expected_pods[0], namespace1, True, 1, 10, 6)
-        utils.check_liveness_data(expected_pods[1]+container_extensions[0], expected_pods[1], namespace1, True, 1, 10, 6)
-        utils.check_liveness_data(expected_pods[1]+container_extensions[1], expected_pods[1], namespace1, True, 1, 10, 6)
-        utils.check_liveness_data(expected_pods[2]+container_extensions[0], expected_pods[2], namespace1, True, 1, 10, 6)
-        utils.check_liveness_data(expected_pods[2]+container_extensions[1], expected_pods[2], namespace1, True, 1, 10, 6)
-        op_rscount = utils.ixia_c_operator_ok(op_rscount)
+        for iter in range(count):
+            print("Iteration: {}".format(iter))
+            print("[Namespace:{}]Deploying KNE topology".format(
+                namespace1
+            ))
+            utils.create_kne_config(namespace1_config, namespace1)
+            utils.ixia_c_pods_ok(namespace1, expected_pods)
+            utils.check_liveness_data('ixia-c', expected_pods[0], namespace1, True, 1, 10, 6)
+            utils.check_liveness_data('gnmi', expected_pods[0], namespace1, True, 1, 10, 6)
+            utils.check_liveness_data('license-server', expected_pods[0], namespace1, True, 1, 10, 6)
+            utils.check_liveness_data(expected_pods[1]+container_extensions[0], expected_pods[1], namespace1, True, 1, 10, 6)
+            utils.check_liveness_data(expected_pods[1]+container_extensions[1], expected_pods[1], namespace1, True, 1, 10, 6)
+            utils.check_liveness_data(expected_pods[2]+container_extensions[0], expected_pods[2], namespace1, True, 1, 10, 6)
+            utils.check_liveness_data(expected_pods[2]+container_extensions[1], expected_pods[2], namespace1, True, 1, 10, 6)
+            op_rscount = utils.ixia_c_operator_ok(op_rscount)
 
-        print("[Namespace:{}]Deleting KNE topology".format(
-            namespace1
-        ))
-        utils.delete_kne_config(namespace1_config, namespace1)
-        utils.ixia_c_pods_ok(namespace1, [])
+            print("[Namespace:{}]Deleting KNE topology".format(
+                namespace1
+            ))
+            utils.delete_kne_config(namespace1_config, namespace1)
+            utils.ixia_c_pods_ok(namespace1, [])
+            utils.wait_for(
+                lambda: utils.topology_deleted(namespace1),
+                'topology deleted',
+                timeout_seconds=30
+            )
         op_rscount = utils.ixia_c_operator_ok(op_rscount)
 
     finally:
