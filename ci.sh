5eb7250c1c204d36af95d52547dd6043026faaee
biplamal <biplab.mal@keysight.com>
2024-02-07 15:19:03 +0530

    bug fix

--- ci.sh
+++ ci.sh
@@ -8,7 +8,7 @@ GO_VERSION=1.17.9
 export DEBIAN_FRONTEND=noninteractive
 
 
-IXIA_C_OPERATOR_IMAGE=keng-operator
+KENG_OPERATOR_IMAGE=keng-operator
 GO_TARGZ=""
 
 GITHUB_REPO="ghcr.io/open-traffic-generator"
@@ -74,7 +74,7 @@ get_docker_build() {
     # Generating docker build using Makefile
     echo "Generating docker build ..."
     export VERSION=$(get_version)
-    export IMAGE_TAG_BASE=${IXIA_C_OPERATOR_IMAGE}
+    export IMAGE_TAG_BASE=${KENG_OPERATOR_IMAGE}
     make docker-build
     docker rmi -f $(docker images | grep '<none>') 2> /dev/null || true
 }
@@ -143,9 +143,9 @@ publish() {
     branch=${1}
     docker images
     version=$(get_version)
-    docker load -i release/gnmi-server.tar.gz
-    github_img="${GITHUB_REPO}/${IXIA_C_OPERATOR_IMAGE}:${version}"
-    github_img_latest="${GITHUB_REPO}/${IXIA_C_OPERATOR_IMAGE}:latest"
+    docker load -i release/keng-operator.tar.gz
+    github_img="${GITHUB_REPO}/${KENG_OPERATOR_IMAGE}:${version}"
+    github_img_latest="${GITHUB_REPO}/${KENG_OPERATOR_IMAGE}:latest"
     docker tag ${img} "${github_img}"
     if [ "$branch" = "main" ]
     then
@@ -167,17 +167,18 @@ publish() {
 }
 
 gen_artifacts() {
+    version=$(get_version)
     mkdir -p ${release}
 
-    rm -rf ./gnmi-server.tar.gz
+    rm -rf ./keng-operator.tar.gz
     rm -rf ${release}/*.tar.gz
-    img="${IXIA_C_OPERATOR_IMAGE}:${version}"
-    github_img="${GITHUB_REPO}/${IXIA_C_OPERATOR_IMAGE}:${version}"
-    git tag $img $github_img
-    docker save ${github_img} | gzip > ${release}/gnmi-server.tar.gz
+    img="${KENG_OPERATOR_IMAGE}:${version}"
+    github_img="${GITHUB_REPO}/${KENG_OPERATOR_IMAGE}:${version}"
+    docker tag $img $github_img
+    docker save ${github_img} | gzip > ${release}/keng-operator.tar.gz
 
     
-    gen_ixia_c_op_dep_yaml "${GITHUB_REPO}/${IXIA_C_OPERATOR_IMAGE}"
+    gen_ixia_c_op_dep_yaml "${GITHUB_REPO}/${KENG_OPERATOR_IMAGE}"
     mv ./ixiatg-operator.yaml ${release}/
     echo "Files in ./release: $(ls -lht ${release})"
 }
@@ -189,12 +190,12 @@ gen_operator_artifacts() {
     rm -rf ${art}/*.yaml
     rm -rf ${art}/*.tar.gz
     mv ./ixiatg-operator.yaml ${art}/
-    docker save ${IXIA_C_OPERATOR_IMAGE}:${version} | gzip > ${art}/keng-operator.tar.gz
+    docker save ${KENG_OPERATOR_IMAGE}:${version} | gzip > ${art}/keng-operator.tar.gz
 }
 
 build() {
     mkdir -p ${art}
-    gen_ixia_c_op_dep_yaml ${IXIA_C_OPERATOR_IMAGE} \
+    gen_ixia_c_op_dep_yaml ${KENG_OPERATOR_IMAGE} \
     && get_docker_build \
     && gen_operator_artifacts ${art}
     version=$(get_version)
